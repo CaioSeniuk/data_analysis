e09b1d40b332a5ef6a77b20ab00e9cfa6050913e
Caio Henrique Goncalves Seniuk <caioseniuk@MacBook-Air-de-Caio.local>
2025-03-30 21:42:29 -0300

    adicionando um novo filtro de tratamento para o arquivo csv

--- tratamento_relatorio_cadop.py
+++ tratamento_relatorio_cadop.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('./Relatorio_cadop.csv', delimiter=';')  
 
-df = df.fillna('N/A')
+df['DDD'] = df['DDD'].apply(lambda x: str(x).replace('.', '') if pd.notnull(x) else x)
+df['Regiao_de_Comercializacao'] = df['Regiao_de_Comercializacao'].apply(lambda x: str(x).replace('.', '') if pd.notnull(x) else x)
+df['Numero'] = df['Numero'].apply(lambda x: str(x).replace('.', '') if pd.notnull(x) else x)
+df['Telefone'] = df['Telefone'].apply(lambda x: str(x).replace('.', '') if pd.notnull(x) else x)
+df['Regiao_de_Comercializacao'] = df['Regiao_de_Comercializacao'].apply(lambda x: str(x).replace('.', '') if pd.notnull(x) else x)
 
 df.to_csv('./Relatorio_cadop_novo.csv', index=False, sep=';') 
